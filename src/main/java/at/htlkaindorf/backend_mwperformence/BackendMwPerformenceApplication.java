97149c1d24504bca597efe90048c9d3394a3b471
Nici0211 <strnib22@htl-kaindorf.at>
2026-03-20 10:59:13 +0100

    first commit

---
+++ src/main/java/at/htlkaindorf/backend_mwperformence/BackendMwPerformenceApplication.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.backend_mwperformence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMwPerformenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendMwPerformenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/htlkaindorf/backend_mwperformence/BackendMwPerformenceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.backend_mwperformence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMwPerformenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
